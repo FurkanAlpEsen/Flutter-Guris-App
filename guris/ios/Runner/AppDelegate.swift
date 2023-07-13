f28390896f981e771ea16e58d74f15096c7b9bfb
Furkan Alp Esen <furkanalpesen@gmail.com>
2023-07-14 00:54:05 +0300

    Local Changes

--- guris/ios/Runner/AppDelegate.swift
+++ guris/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBp_fd5fHw1euqmzytv8NNQIRUsOa-koNg")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
